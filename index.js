e4b8806cfc4628d4b1c7fdcf4eed2f3b90be07ea
Ali Yetgin <yetgin.ali@outlook.com>
2022-12-15 19:53:52 +0300

    Do reset the board, resize board and fiil the board with random colors buttons

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const container = document.querySelector(".grid-container");
 
 
-for (let i = 0; i < 256; i++) {
-        const sqr = document.createElement("div");
-        sqr.classList.add("square");
-        container.append(sqr);
+const createGrid = (col, row) => {
+        for (let i = 0; i < (col * row); i++) {
+                const sqr = document.createElement("div");
+                sqr.classList.add("square");
+                container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+                container.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+                container.append(sqr);
+        }
+
+        
 }
 
-container.setAttribute("style", "grid-template-columns: repeat(16, 1fr); ");
-
-
-const squares = document.querySelectorAll(".square");
-
-/* for (square of squares) {
-        square.addEventListener('mouseover', (e) => {
-                square.style.backgroundColor = newColor();
-                console.log(this);
-        });
+const addColor = () => {
+        const squares = document.querySelectorAll(".square"); 
+        squares.forEach(square => {
+                square.addEventListener('mouseover', (e) => {
+                square.style.backgroundColor = "#161616";
+        })
+        }); 
 }
- */
 
-squares.forEach(square => {
-        square.addEventListener('mouseover', (e) => {
-        square.style.backgroundColor = newColor();
-})
-});
+createGrid(16, 16);
+addColor();
+
 
 function newColor() {
         const r = Math.floor(Math.random() * 255);
@@ -34,4 +34,36 @@ function newColor() {
         return `rgb(${r}, ${g}, ${b})`;  
 }
 
+const resize = document.querySelector(".resize");
+resize.addEventListener("click", btn => {
+       const size = prompt("Enter size of your new grid: (if you want 64x64 enter 64)");
+       container.textContent = '';
+       const footerSizeText = document.querySelector(".footer p");
+       footerSizeText .textContent = `Current size of the board: ${size}x${size}`;
+       createGrid(size, size);
+       addColor(); 
+});
+
+const randColor = document.querySelector(".rand");
+randColor.addEventListener("click", btn => {
+        const squares = document.querySelectorAll(".square"); 
+        squares.forEach(square => {
+                squares.forEach(square => {   
+                        square.style.backgroundColor = "#FFFFFF";
+        }); 
+                square.addEventListener('mouseover', (e) => {
+                square.style.backgroundColor = newColor();
+        })
+        }); 
+})
+
+
+
+const reset = document.querySelector(".reset");
+reset.addEventListener("click", btn => {
+        const squares = document.querySelectorAll(".square"); 
+        squares.forEach(square => {   
+                square.style.backgroundColor = "#FFFFFF";
+}); 
+})
 
